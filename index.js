dd927c1324333c10fc6246b724e1f89ffadc0022
Robbie Jaeger <jaeger.rob@gmail.com>
2016-10-22 22:56:19 -0600

    Add CSS

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.set('view engine', 'handlebars');
 app.set('port', process.env.PORT || 3000);
 
 app.get('/', function(req, res){
-  res.render('home')
+  res.render('home');
 });
 
 // custom 404 page

68f665a87b2bc71951f3ce003139ab6892e116f1
Robbie Jaeger <jaeger.rob@gmail.com>
2016-11-25 11:48:50 -0700

    Remove whitespace in index JS file

--- index.js
+++ index.js
@@ -31,14 +31,11 @@ app.get('/registry', function(req, res){
   res.render('registry');
 });
 
-
-// custom 404 page
 app.use(function(req, res){
   res.status(404);
   res.render('404');
 });
 
-// custom 500 page
 app.use(function(err, req, res, next){
   console.error(err.stack);
   res.status(500);
